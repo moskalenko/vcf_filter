0e1c146ed28fc216d26515ccaa5d0339ef8d52b1
Oleksandr Moskalenko <om@rc.ufl.edu>
2015-01-28 10:49:20 -0500

    Add a basic mode to the filtering script, which processes lines as simple text

--- vcf_filter.py
+++ vcf_filter.py
@@ -19,6 +19,7 @@ def get_arguments():
 #    parser.add_argument('-z', '--zip', action='store_true', default=False, help="Compress output files with gzip")
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help="Verbose output")
     parser.add_argument('-d', '--debug', action='store_true', default=False, help="Debug the script")
+    parser.add_argument('-b', '--basic', action='store_true', default=False, help="Basic Mode - do not use PyVCF; treat as plain text")
     parser.add_argument('--version', action='version', version='%(prog)s Version: {version}'.format(version=__version))
     args = parser.parse_args()
     if (not args.infile):
@@ -33,35 +34,38 @@ def read_snp_list(args):
     snps = frozenset( x.strip() for x in open(args.infile) )
     return snps
 
-def filter_vcf_files(args, vcf_files, snp_list):
-    for input_file in vcf_files:
-        if args.verbose:
-            print "Processing file {0}".format(input_file)
-        suffix = args.suffix
-        file_basename = os.path.split(input_file)[1]
-        file_name_base = file_basename.split('.vcf')[0]
-        outfile_name = file_name_base + '_' + args.suffix + '.vcf'
-        if args.outdir:
-            outfile_name = file_name_base + '_' + args.suffix + '.vcf'
-            outpath = os.path.join(args.outdir, outfile_name)
-        else:
-            outpath = outfile_name
-        if args.verbose:
-            print "Writing output to {0}".format(outpath)
-        if file_basename.endswith('gz'):
-            input_fh = open(input_file, 'rb')
-        else:
-            input_fh = open(input_file,'r')
-        v_reader = vcf.Reader(input_fh)
-        output_fh = open(outpath, 'w')
-        v_writer = vcf.Writer(output_fh, v_reader)
-        for record in v_reader:
+
+def filter_vcf_files_basic(args, input_fh, output_fh, snp_set):
+    header_lines = True
+    for line in input_fh:
+        if args.debug:
+            print line
+        if header_lines:
+            if line.startswith("#"):
+                output_fh.write(line)
+                continue
+            else:
+                header_lines = False
+        record_id = line.split('\t')[2]
+        if record_id in snp_set:
+            output_fh.write(line)
+    input_fh.close()
+    output_fh.close()
+
+def filter_vcf_files_pyvcf(args, input_fh, output_fh, snp_set):
+    v_reader = vcf.Reader(input_fh)
+    v_writer = vcf.Writer(output_fh, v_reader)
+    for record in v_reader:
+        if args.debug:
+            print record
+        if record.ID in snp_set:
+            if args.verbose:
+                print "Found matching ID: {0}".format(record.ID)
             if args.debug:
                 print "{0}\t{1}".format(record.CHROM, record.ID, record.INFO)
-            if record.ID in snp_list:
-                if args.verbose:
-                    print "Found matching ID: {0}".format(record.ID)
-                v_writer.write_record(record)
+            v_writer.write_record(record)
+    input_fh.close()
+    output_fh.close()
 
 if __name__=='__main__':
     logger = logging.getLogger(__name__)
@@ -72,30 +76,52 @@ if __name__=='__main__':
     logger.addHandler(ch)
     logger.setLevel('DEBUG')
     if sys.version_info < (2,7,0):
-        print ("You need python 2.7 or later to run this script.")
+        print ("\nYou need python 2.7 or later to run this script.")
         sys.exit(1)
     args = get_arguments()
     input_types = ['gz', 'vcf']
     try:
         vcf_list_fh = open(args.infile, 'r')
     except IOError:
-        print 'Cannot open variant list file', arg
+        print '\nCannot open variant list file', arg
     vcf_files = []
     if len(args.infiles) > 0:
         if args.debug:
-            print "Provided input files:\n"
-            print args.infiles
+            print "\nProvided input files:"
+            print "\t", args.infiles
         for input_file in args.infiles:
             for input_type in input_types:
                 if input_file.endswith(input_type):
                     vcf_files.append(input_file)
     else:
         sys.exit("\nAt least one input file (.vcf or .vcf.gz) must be provided\n")
-    snps = read_snp_list(args)
+    snp_set = read_snp_list(args)
     if args.verbose:
-        print("Read {0} variants from {1}".format(len(snps), args.infile))
+        print("\nRead {0} variants from {1}".format(len(snp_set), args.infile))
     list_of_input_vcf_files = ", ".join(vcf_files)
     if args.verbose:
-        print("\nFiles to process:\n{0}".format(list_of_input_vcf_files))
-    filter_vcf_files(args, vcf_files, snps)
+        print("\nFiles to process:\n\t{0}".format(list_of_input_vcf_files))
+    for input_file in vcf_files:
+        if args.verbose:
+            print "\nProcessing file {0}".format(input_file)
+        suffix = args.suffix
+        file_basename = os.path.split(input_file)[1]
+        file_name_base = file_basename.split('.vcf')[0]
+        outfile_name = file_name_base + '_' + args.suffix + '.vcf'
+        if args.outdir:
+            outfile_name = file_name_base + '_' + args.suffix + '.vcf'
+            outpath = os.path.join(args.outdir, outfile_name)
+        else:
+            outpath = outfile_name
+        output_fh = open(outpath, 'w')
+        if args.verbose:
+            print "\nWriting output to {0}".format(outpath)
+        if file_basename.endswith('gz'):
+            input_fh = gzip.open(input_file, 'rb')
+        else:
+            input_fh = open(input_file,'r')
+        if args.basic:
+            filter_vcf_files_basic(args, input_fh, output_fh, snp_set)
+        else:
+            filter_vcf_files_pyvcf(args, input_fh, output_fh, snp_set)
     sys.exit(0)
